496220eb5fd695d3e2988218cf058a356c961336
Dmitriy <dmitriiveselkov@gmail.com>
2024-08-25 20:44:03 +0300

    Ипр JavaEE

---
+++ ipr-ejb/src/main/java/ru/veselkov/service/RemoteCallService.java
@@ -0,0 +1,29 @@
+package ru.veselkov.service;
+
+import jakarta.ejb.EJB;
+import jakarta.ejb.Stateless;
+import ru.veselkov.iprremote.service.RemoteServiceIpr;
+import ru.veselkov.iprremote.service.ServiceBean;
+
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Hashtable;
+
+@Stateless
+public class RemoteCallService {
+
+    public void call() throws NamingException {
+        System.out.println("ipr call()");
+        final Hashtable jndiProperties = new Hashtable();
+        jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
+        jndiProperties.put(Context.PROVIDER_URL, "remote+http://localhost:8081/");
+
+        final Context context = new InitialContext(jndiProperties);
+        RemoteServiceIpr remoteServiceIpr = (RemoteServiceIpr) context.lookup("ejb:/target/ServiceBean!" + RemoteServiceIpr.class.getName());
+        remoteServiceIpr.callRemote();
+        System.out.println("ipr call() end");
+    }
+
+}
--- ipr-web/src/main/java/ru/veselkov/ServiceController.java
+++ ipr-web/src/main/java/ru/veselkov/ServiceController.java
@@ -8,13 +8,19 @@ import jakarta.ws.rs.core.Response;
 import ru.veselkov.dto.CustomerDto;
 import ru.veselkov.dto.ProductDto;
 import ru.veselkov.dto.RegistrationUser;
+import ru.veselkov.service.RemoteCallService;
 import ru.veselkov.service.SimpleService;
+
+import javax.naming.NamingException;
 
 @Path("/service")
 public class ServiceController {
 
     @EJB(lookup = "java:global/ipr/ipr-ejb/SimpleServiceBean!ru.veselkov.service.SimpleService")
     SimpleService service;
+
+    @EJB
+    private RemoteCallService remoteCallService;
 
     @Path("/create/customer")
     @POST
@@ -40,5 +46,17 @@ public class ServiceController {
     public Response createProduct(@PathParam("id") int id) {
         CustomerDto customerDto = service.findById(id);
         return Response.ok(customerDto).build();
+    }
+
+    @Path("/remote")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response remote() {
+        try {
+            remoteCallService.call();
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+        return Response.ok().build();
     }
 }
